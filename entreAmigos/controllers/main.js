d6fdb11feadff404f64a5ccf68846b5bfbc958f9
akpaz <akpaz@udesa.edu.ar>
2024-04-05 15:13:12 -0300

    planteo de controladores

---
+++ entreAmigos/controllers/main.js
@@ -0,0 +1,27 @@
+
+
+const mainController = {
+    index: function (req, res) {
+        
+    },
+    login: function (req, res) {
+        
+    },
+    register: function (req, res) {
+        
+    },
+    detalleProducto: function (req, res) {
+        
+    },
+    profile: function (req, res) {
+        
+    },
+    addProduct: function (req, res) {
+        
+    },
+    profileEdit: function (req, res) {
+        
+    }
+}
+
+module.exports = mainController;
