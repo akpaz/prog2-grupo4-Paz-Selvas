e2816fff43e00a99c6414e2f457fc870e7a90797
camuxsel <cselvas@udesa.edu.ar>
2024-04-05 15:06:39 -0300

    escribi las rutas necesarias, faltan controllers

---
+++ entreAmigos/routes/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router;
+
+router.get('/',);
+
+router.get('/login');
+
+router.get('/register',);
+
+router.get('/product/:product',);
+
+router.get('/profile', );
+
+router.get('/product-add/:product', );
+
+
+module.exports = router;
