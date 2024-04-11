09e3a37bcb6d75a15d037fa0b5427199b84fba60
camuxsel <cselvas@udesa.edu.ar>
2024-04-11 10:26:43 -0300

    modulo de datos del usuario

---
+++ entreAmigos/db/main.js
@@ -0,0 +1,9 @@
+const datos = {
+    usuario : {email:'pedroperez@gmail.com', 
+            usuario:'pedroperez', 
+            password:'hola1234', 
+            nacimiento:'12/01/2002', 
+            dni:42560978, 
+            foto:'/images/pedroperez'},
+    productos : []
+};
