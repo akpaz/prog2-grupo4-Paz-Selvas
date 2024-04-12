0ec9d42c2331e4a96d24ab3389f5f635638d7ac4
akpaz <akpaz@udesa.edu.ar>
2024-04-12 15:20:27 -0300

    creacion de tablas de usuarios y productos en archivo sql

--- entreAmigos/db/main.sql
+++ entreAmigos/db/main.sql
@@ -1,3 +1,30 @@
 create schema piGrupo4EntreAmigos;
 
 use piGrupo4EntreAmigos;
+
+create table Usuarios (
+    id int unsigned primary key auto_increment,
+    nombreUsuario varchar(50) unique,
+	email varchar(50) unique,
+    contrasena varchar(50),
+	fechaDeNacimiento date,
+    dni bigint unique,
+    fotoPerfil varchar(100),
+    createdAt timestamp default current_timestamp,
+	updatedAt timestamp default current_timestamp on update current_timestamp,
+	deletedAt timestamp null on update current_timestamp
+);
+
+create table Productos (
+    id int unsigned primary key auto_increment,
+    dUsuarioPropietario int unsigned,
+	imagen varchar(100),
+	nombreProducto varchar(50),
+	descripcionProducto varchar(500),
+	createdAt timestamp default current_timestamp,
+	updatedAt timestamp default current_timestamp on update current_timestamp,
+	deletedAt timestamp null on update current_timestamp,
+    
+    foreign key (idUsuarioPropietario) references Usuarios(id)
+);
+
