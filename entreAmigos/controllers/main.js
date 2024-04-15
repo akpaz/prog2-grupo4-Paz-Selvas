49a04362a8810f6e321aa72bc90f791cb227a039
akpaz <akpaz@udesa.edu.ar>
2024-04-15 15:15:59 -0300

    modificacion del ejs de profile y header logueado

--- entreAmigos/controllers/main.js
+++ entreAmigos/controllers/main.js
@@ -2,6 +2,7 @@ const datosProductos = require('../db/main');
 const usuario = datosProductos.usuario;
 const productos = datosProductos.productos;
 
+
 const mainController = {
     index: function (req, res) {
         res.render('index', {productos:productos})
@@ -23,7 +24,7 @@ const mainController = {
         res.render('product', {idProducto : idProducto, producto : producto});
     },
     profile: function (req, res) {
-        res.render('profile');
+        res.render('profile', {usuario:usuario, productos:productos});
     },
     addProduct: function (req, res) {
         res.render('product-add');
