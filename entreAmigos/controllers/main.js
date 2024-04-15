edc44905abb405f013527e98039066e97c02f13a
camuxsel <cselvas@udesa.edu.ar>
2024-04-14 21:02:20 -0300

    agregue footer en mis views, trabaje con css register, un par de cositas mas

--- entreAmigos/controllers/main.js
+++ entreAmigos/controllers/main.js
@@ -7,7 +7,7 @@ const mainController = {
         res.render('index')
     },
     login: function (req, res) {
-        
+        res.render('login');
     },
     register: function (req, res) {
         res.render('register');
@@ -23,17 +23,26 @@ const mainController = {
         res.render('product', {idProducto : idProducto, producto : producto});
     },
     profile: function (req, res) {
-        
+        res.render('profile');
     },
     addProduct: function (req, res) {
-        
+        res.render('product-add');
     },
     profileEdit: function (req, res) {
-        
+        res.render('profile-edit');
     },
     busqueda:function (req, res) {
-        
+        let busqueda = req.params.busqueda;
+        let resultados = null;
+        for (let i = 0; i < productos.length; i++) {
+            if (productos[i].nombre == busqueda) {
+                resultados = productos[i]
+            } else{
+                resultados =   `Lo sentimos, no hay resultados para ${busqueda}!`
+            };
+        };
+        res.render('search-results', {resultados : resultados});
     }
-}
+};
 
 module.exports = mainController;
