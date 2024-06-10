e2929363eca53fa65df4197fc43423fb400bc180
akpaz <akpaz@udesa.edu.ar>
2024-06-10 16:27:55 -0300

    arreglo de merge

--- entreAmigos/controllers/usuarios.js
+++ entreAmigos/controllers/usuarios.js
@@ -1,6 +1,6 @@
 const db = require('../database/models');
 const bcrypt = require('bcryptjs');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const datosProductos = require('../db/main');
 const usuario = datosProductos.usuario;
@@ -10,7 +10,7 @@ const usuariosController = {
     register: function (req, res) {
         if (req.session.user !== undefined) {
             return res.redirect('/');
-        }else{
+        } else {
             return res.render('register');
         }
     },
@@ -27,74 +27,71 @@ const usuariosController = {
                 fotoPerfil: req.body.fotoPerfil
             });
             return res.redirect('/');
-        }else{
+        } else {
             // hay errores, volvemos al formulario mostrando los errores
-            return res.render('register', {errors: errors.mapped(), old: req.body})
+            return res.render('register', { errors: errors.mapped(), old: req.body })
         }
-        
+
         // res.send(req.body);
     },
     login: function (req, res) {
         if (req.session.user !== undefined) {
             return res.redirect('/');
-        } else{
+        } else {
             res.render('login');
         }
     },
-    processLogin: function(req, res) {
+    processLogin: function (req, res) {
         let errors = validationResult(req);
         if (errors.isEmpty()) {
             // No hay errores, avanzamos con el código normal
             db.Usuario.findOne({
-                where: [{email: req.body.email}]
+                where: [{ email: req.body.email }]
             })
-            .then(function(usuarioLogueado) {
-                // return res.send(usuarioLogueado)
-                // lo ponemos en session
-                req.session.user = {
-                    email: usuarioLogueado.email,
-                    userName: usuarioLogueado.nombreUsuario,
-                    id: usuarioLogueado.id
-<<<<<<< HEAD
-                }                
-=======
-                }
-                //console.log(user);
-                
->>>>>>> 7d231a03270acec7dffb75477eae3ee3b17c6fce
-                //Preguntar si el usuario tildó el checkbox para recordarlo
-                if(req.body.recordar !== undefined){
-                    res.cookie('usuarioGuardado', req.session.user, {maxAge: 1000*60*1000000})
-                }
-               
-                if (bcrypt.compareSync(req.body.password, usuarioLogueado.contrasena)) {
-                    return res.redirect('/');
-                } else{
-                    return res.render('login',{errors: errors.mapped(), old: req.body})
-                }
-            })
+                .then(function (usuarioLogueado) {
+                    // return res.send(usuarioLogueado)
+                    // lo ponemos en session
+                    req.session.user = {
+                        email: usuarioLogueado.email,
+                        userName: usuarioLogueado.nombreUsuario,
+                        id: usuarioLogueado.id
+                    }
+                };
+            //console.log(user);
+
+            //Preguntar si el usuario tildó el checkbox para recordarlo
+            if (req.body.recordar !== undefined) {
+                res.cookie('usuarioGuardado', req.session.user, { maxAge: 1000 * 60 * 1000000 })
+            };
+
+            if (bcrypt.compareSync(req.body.password, usuarioLogueado.contrasena)) {
+                return res.redirect('/');
+            } else {
+                return res.render('login', { errors: errors.mapped(), old: req.body })
+            }
             .catch(function (e) {
                 console.log(e);
             })
-        }else{
-            // Si hay errores, volvemos al formulario con los mensajes
-            return res.render('login',{errors: errors.mapped(), old: req.body});
-        }   
+}
+        else {
+    // Si hay errores, volvemos al formulario con los mensajes
+    return res.render('login', { errors: errors.mapped(), old: req.body });
+}   
     },
-    logout: function(req, res){
-        // destruimos la session
-        req.session.destroy();
-        // destruimos la cookie
-        res.clearCookie('usuarioGuardado');
-        //redirigimos al usuario al inicio
-        return res.redirect('/')
-    },
-    profile: function (req, res) {
-        return res.render('profile', { usuario: usuario, productos: productos });
-    },
-    profileEdit: function (req, res) {
-        return res.render('profile-edit', { usuario: usuario });
-    }
+logout: function(req, res) {
+    // destruimos la session
+    req.session.destroy();
+    // destruimos la cookie
+    res.clearCookie('usuarioGuardado');
+    //redirigimos al usuario al inicio
+    return res.redirect('/')
+},
+profile: function(req, res) {
+    return res.render('profile', { usuario: usuario, productos: productos });
+},
+profileEdit: function (req, res) {
+    return res.render('profile-edit', { usuario: usuario });
+}
 }
 
 module.exports = usuariosController;
