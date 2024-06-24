541d7ffe0babcbf7b2239f5e2f7d6d8357f5aa48
camuxsel <cselvas@udesa.edu.ar>
2024-06-24 17:28:30 -0300

    scrpit sql para que tengan la base de datos, las contras de los usuarios son 123456 si quieren probarlas ;)

--- entreAmigos/db/main.sql
+++ entreAmigos/db/main.sql
@@ -47,76 +47,53 @@ create table Comentarios (
     foreign key (idUsuario) references Usuarios(id)
 );
 
--- ahora hacemos los registros pedidos
 
--- agregamos 5 registros en usuarios
 
 insert into Usuarios
-values(default,'zacEfron','zacefron@gmail.com','hola123','2003-11-23',43789456,'zacEfron.png',default,default,default),
-	(default,'tomHolland','tomholland@gmail.com','zendayateamo','2004-08-21',43890456,'tomHolland.png',default,default,default),
-	(default,'margotRobbie','margotrobbie@gmail.com','barbie1','2000-07-03',34274682,'margotRobbie.png',default,default,default),
-	(default,'emmaWatson','emmawatson@gmail.com','hermione123','2002-09-02',3784875,'emmaWatson.png',default,default,default),
-	(default,'jenniferAnniston','jenniferanniston@gmail.com','hello123','2001-06-17',4729384,'jenniferAnniston.png',default,default,default);
+values(default,'Cami Selvas','camibelensel@gmail.com','$2a$10$MBu4vj2.9WqINzgKUv2FwuBeL0J5phAdiPPQaFOGtqukzKPqUWRwK','2004-12-11',46204710,'camiSelvas.png',default,default,default),
+	(default,'aledh','ale@dh.com','$2a$10$KA.S7NY.6xU9yY0eumDzb.hV41zveBmaMUWAns.IHLibIRore9ELe','2024-04-10',12345678,'ale.png',default,default,default),
+	(default,'agus_paz','akpaz@udesa.edu.ar','$2a$10$iKcqc/jYPeeUVQH5aScNO.5jDflverEcr3/ZaMmyY0Ownro4keB6K','2004-08-28',45897304,'agusPaz.png',default,default,default),
+	(default,'diego','diego123@gmail.com','$2a$10$dfrCLaqk8I81idOYx.3M5OwwLkNMkylLqU/JWclmBTALV234X.cze','1994-03-21',64950334,'diego.png',default,default,default),
+	(default,'camii','camilabelen30@gmail.com','$2a$10$Mx0RyyuHgGeVruvFyzLd9.M6GISkr0RtjYtyy7wgrOpWU11VWFW7m','1996-10-14',35504213,'ccc.png',default,default,default),
+	(default,'Mariaa','maria@gmail.com','$2a$10$/Qs8kFY0Ro7l3MLDoFKda./Pl07b82fGBh.6EGkn.qks9oskuDRgq','1997-01-23',35264873,'maria.png',default,default,default),
+	(default,'Juancito','juan@gmail.com','$2a$10$Jn3BUtNefKpeNREKEGEjpuvZAKs/vAJiEBYJQ98N.nlHPaCN.JobC','2004-12-11',46376486,'juan.png',default,default,default),
+	(default,'- Rafa Nadal -','rafita@gmail.com','$2a$10$d6WgV.cs.GipPw8mhaaLv.4qauECZaysQwrUVsr03lZo87tIPL7VW','2004-12-11',36485729,'rafaNadal.png',default,default,default);
 
--- agregamos 10 productos
 
 insert into Productos
-values(default,1,'toby.png','Toby','Toby tiene 3 años y es muy amigable y sociable. Está buscando un hogar con mucho cariño.',3,'perro','masculino','sociable y amigable',default,default,default),
-	(default,1,'peter.png','Peter','Peter tiene 12 años y es una cruza con Beagle. Se lleva muy bien con los niños.',12,'perro','masculino','leal y amistoso',default,default,default),
-	(default,1,'luna.png','Luna','Reservada pero cariñosa, buscando un lugar tranquilo para compartir afecto.',3,'gato','femenino','sensible y paciente',default,default,default),
-	(default, 2, 'oliver.png', 'Oliver', 'Curioso observador que busca un hogar donde pueda explorar sin límites.', 12, 'gato', 'masculino', 'independiente y curioso', default, default, default),
-	(default, 2, 'simba.png', 'Simba', 'Ideal si necesitas un amigo tranquilo y relajado que disfruta de largas siestas a tu lado.', 6, 'gato', 'masculino', 'reservado y tranquilo', default, default, default),
-	(default, 1, 'molly.png', 'Molly', 'Energética y amigable, Molly está lista para acompañarte en todas tus aventuras. ', 1, 'perro', 'femenino', 'alegre y juguetona', default, default, default),
-	(default, 1, 'bella.png', 'Bella', 'Amable y compasivo, ideal para alguien que valore la compañía tranquila.', 2, 'perro', 'femenino', 'independiente y amigable', default, default, default),
-	(default, 1, 'charlie.png', 'Charlie', 'Leal y protector, esperando ansiosamente ser parte de una familia amorosa', 4, 'perro', 'masculino', 'inteligente y afectivo', default, default, default),
-	(default, 1, 'tyson.png', 'Tyson', 'Compañero de abarazos que te reconforta con su calidez y afecto', 2, 'perro', 'masculino', 'cariñoso y atento', default, default, default),
-	(default, 2, 'moka.png', 'Moka', 'Tímida al principio, pero una vez que confía, es leal y afectuosa.', 3, 'gato', 'femenino', 'amigable y atenta', default, default, default);
+values(default,1,'luna.png','Luna','Luna está buscando un hogar con mucho cariño para dar! <3',3,'Gato','femenino','Sensible y paciente',default,default,default),
+	(default, 2, 'tyson.png', 'Tyson', 'Tyson está buscando acompañar a alguien y disfrutar juntos de la vida!', 2, 'Perro pug', 'masculino', 'Cariñoso y atento', default, default, default),
+	(default,1,'bianca.png','Bianca','Bianca está buscando con quien compartir sus últimos años de vida. Es una fiel compañera que nunca te hará sentir solo <3',12,'Perro caniche','Femenino','Leal y obediente',default,default,default),
+	(default, 6, 'oliver.png', 'Oliver', 'Oliver es un gato encantador y lleno de energía, ideal para una familia que busca un compañero juguetón y cariñoso. ', 3, 'Gato naranja', 'Masculino', 'Amoroso y juguetón', default, default, default),
+	(default, 6, 'charlie.png', 'Charlie', 'Charlie es un perro increíblemente dulce y leal que está buscando una familia amorosa para siempre. Es un perro activo y enérgico, perfecto para una familia que disfrute de paseos al aire libre y juegos en el parque.', 7, 'Perro', 'Masculino', 'Inteligente y afectivo', default, default, default),
+	(default, 1, 'simba.png', 'Simba', 'Un gatito muy amoroso que busca formar parte de un hogar con mucho cariño. No esperes más a darle la vida que se merece!', 4, 'Gato atigrado', 'Masculino', 'Reservado y tranquilo', default, default, default),
+	(default, 2, 'moka.png', 'Moka', 'Tímida al principio, pero una vez que confía, es leal y afectuosa.', 3, 'Gato manchado', 'Femenino', 'Amigable y atenta', default, default, default),
+	(default, 3, 'bella.png', 'Bella', 'Una perrita muy delicada que le gusta pasear en las mañanas y descansar en las tardes.', 7, 'Perro Yorkshire', 'Femenino', 'Independiente y amigable', default, default, default),
+	(default, 3, 'molly.png', 'Molly', 'Molly siempre busca darte los mejores cariños y darte calor con su pelo suave. Ideal para descansar juntos viendo una peli.', 5, 'Perro', 'Femenino', 'Alegre y juguetona', default, default, default);
 
--- comentarios
 insert into Comentarios
-	values(default, 1,1, 'Que tierno<3', default, default, default),
-	(default, 1, 2, 'Quiero adoptarlo ya!', default, default, default),
-	(default, 1, 3, 'Se va allevar muy bien con mi perrito.', default, default, default),
-	(default, 1, 4, 'Tengo un caniche y se porta re bien.', default, default, default),
-	(default, 1, 5, 'Que belleza.', default, default, default),
-	(default, 2, 1, 'Es hermoso!', default, default, default),
-	(default, 2, 2, 'Que ternura.', default, default, default),
-	(default, 2, 3, 'Parece un Jack Russsell!', default, default, default),
-	(default, 2, 4, 'Que lindas manchas.', default, default, default),
-	(default, 3, 1, 'Es hermosa!', default, default, default),
-	(default, 3, 2, 'Siempre quise un gatito!', default, default, default),
-	(default, 3, 3, 'La mejor compañia', default, default, default),
-	(default, 4, 1, 'Es muy tierno!', default, default, default),
-	(default, 4, 2, 'Sigue en adopcion?', default, default, default),
-	(default, 4, 4, 'Es muy lindo', default, default, default),
-	(default, 4, 5, 'Me lo quiero llevar a casa!', default, default, default),
-	(default, 5, 1, 'Es igual al de la peli! jajaja', default, default, default),
-	(default, 5, 2, 'Es hermoso!!', default, default, default),
-	(default, 5, 3, 'Mi gatito es igual! Se llevarian re bien', default, default, default),
-	(default, 5, 4, 'Ay es muy tierno', default, default, default),
-	(default, 5, 5, '<3 <3', default, default, default),
-	(default, 6, 1, 'Qué tierna <3', default, default, default),
-	(default, 6, 2, 'Es hermosa!', default, default, default),
-	(default, 6, 3, 'Que chiquita, una ternura', default, default, default),
-	(default, 6, 4, 'Sigue en adopcion?', default, default, default),
-	(default, 6, 5, 'Que belleza.', default, default, default),
-	(default, 7, 1, 'Es bellisima!', default, default, default),
-	(default, 7, 2, 'Es hermosa!', default, default, default),
-	(default, 7, 3, 'Que lindos los ojitos!', default, default, default),
-	(default, 7, 4, 'Ternura total', default, default, default),
-	(default, 7, 5, 'Que belleza.', default, default, default),
-	(default, 8, 1, 'Gran nombre para un gran perro!', default, default, default),
-	(default, 8, 2, 'Es muy tierno', default, default, default),
-	(default, 8, 3, 'Lo puedo pasar a ver?', default, default, default),
-	(default, 8, 4, 'Tengo uno parecido!', default, default, default),
-	(default, 8, 5, 'Que lindos los ojitos!', default, default, default),
-	(default, 9, 1, 'Qué tierno <3', default, default, default),
-	(default, 9, 2, 'Mi amiga tiene uno igual!', default, default, default),
-	(default, 9, 3, 'Se lleva bien con otros perritos?', default, default, default),
-	(default, 9, 4, 'Que original su nombre! Me encanta', default, default, default),
-	(default, 9, 5, 'Hermoso <3', default, default, default),
-	(default, 10, 1, 'La quiero adoptar ya!', default, default, default),
-	(default, 10, 2, 'Me la quiero traer a casa, es muy tierna', default, default, default),
-	(default, 10, 3, 'Que ternura', default, default, default),
-	(default, 10, 4, 'Ay es hermosa!', default, default, default),
-	(default, 10, 5, 'Qué tierno <3', default, default, default);
+	(default, 'Que bonita que es <3', 1, 2, default, default, default),
+	(default, 'Que belleza, la quiero', 1, 2, default, default, default),
+	(default, 'Esta vacunada?', 1, 1, default, default, default),
+	(default, 'Cuantos años tiene? ', 1, 3, default, default, default),
+	(default, 'Puedo pasar a verla?', 1,4, default, default, default),
+	(default, 'Cuantos años tiene? ', 2,1, default, default, default),
+	(default, 'Ya se puede adoptar?', 2,1, default, default, default),
+	(default, 'Ya se puede adoptar?', 1,2, default, default, default),
+	(default, 'Sigue disponible????', 2,5, default, default, default),
+	(default, 'Ya se puede adoptar?', 3,1, default, default, default),
+	(default, 'Que belleza, la quiero', 3,5, default, default, default),
+	(default, 'Disponible ya! <3', 4,6, default, default, default),
+	(default, 'Que linda!', 3,6, default, default, default),
+	(default, 'Se ve tan bueno...', 5,1, default, default, default),
+	(default, 'Queee lindo que es por favorrrr', 6,6, default, default, default),
+	(default, 'Se lleva bien con niños?', 3,2, default, default, default),
+	(default, 'Que gordito lindo...', 2,4, default, default, default),
+	(default, 'Lo puedo pasar a ver?', 6,3, default, default, default),
+	(default, 'Una amiga está interesada!', 3,3, default, default, default),
+	(default, 'Que tierna, se ve muy compañera.', 7,6, default, default, default),
+	(default, 'Mi prima tiene una Yorkshire, es muy amorosa...', 8,6, default, default, default),
+	(default, 'Que agradable se ve', 9,7, default, default, default),
+	(default, 'Lo puedo conocer primero?', 4,7, default, default, default),
+	(default, 'Que tierno<3', 1,1, default, default, default);
+
